ef97cf0239f25d212ca31b6f71ac6d6a86bd4738
dlavrushko-capgemini <danylo.lavrushko@capgemini.com>
2025-11-04 15:36:38 +0200

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+## prerequisites
+1. Python 3.12
+2. uv
+
+## setup
+1. Copy `.env.example` to `.env` and fill missing values for vars.
+2. Try different models.
+   - for `openai.gpt-4o`, application works correctly
+   - for `gemini-2.5-pro`, tool calls do not work
---
+++ main.py
@@ -0,0 +1,65 @@
+import asyncio
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from langchain_core.tools import BaseTool
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mcp_client = MultiServerMCPClient(
+    {
+        "n8n-mcp": {
+            "command": "npx",
+            "args": ["n8n-mcp@2.20.6"],
+            "env": {
+                "MCP_MODE": "stdio",
+                "LOG_LEVEL": "error",
+                "DISABLE_CONSOLE_OUTPUT": "true",
+                "N8N_MCP_TELEMETRY_DISABLED": "true",
+            },
+            "transport": "stdio",
+        },
+    }
+)
+
+
+chat = ChatOpenAI(
+    model=os.environ["OPENAI_MODEL"],
+    base_url=os.environ["OPENAI_BASE_URL"],
+    api_key=os.environ["OPENAI_API_KEY"],
+    streaming=False,
+    disable_streaming=True,
+    temperature=0.0,
+    max_tokens=4096,
+    timeout=60,
+)
+
+
+async def get_tools() -> list[BaseTool]:
+    tools = await mcp_client.get_tools()
+    return tools
+
+async def main():
+    tools = await get_tools()
+    
+    agent = create_react_agent(
+        model=chat,
+        tools=tools,
+        prompt="""\
+        You are an expert in n8n automation software.
+        Do not ask follow-up questions or request clarification.
+        **Start**: Call `tools_documentation()` for best practices.""",
+    )
+
+    state = {"messages": [HumanMessage(content="Create a n8n workflow JSON that sends an email on manual trigger.")]}
+    
+    response = await agent.ainvoke(state)
+
+    print("Agent Response:", response["messages"][-1].content)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
